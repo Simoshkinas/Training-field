2da9ee7c5d9f24652014183476a5fa27a93d6bd7
Simonas Pranaitis <simopnas.pranaitis@gmail.com>
2023-02-27 11:38:59 +0200

    Person list implementation

---
+++ FieldDayWithOOP3/Person.cs
@@ -0,0 +1,50 @@
+﻿namespace FieldDayWithOOP3
+{
+    internal class Person
+    {
+        #region Fields
+        private string _firstName, _lastName;
+        #endregion
+        #region Properties
+        internal int Age { get; set; }
+        internal string Gender { get; set; } 
+        #endregion
+        #region Constructor
+        public Person(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+        #endregion
+        #region Methods
+        /// <summary>
+        /// Gives greeting to the console
+        /// </summary>
+        internal void Greetings()
+        {
+            Console.WriteLine($"Hello, my name is {_firstName} {_lastName}");
+        }
+        /// <summary>
+        /// Displays ful info of a person to the console
+        /// </summary>
+        internal void GetFullInfo()
+        {
+            Greetings();
+            Console.WriteLine($"My Age: {Age}");
+            Console.WriteLine($"My Gender: {Gender}");
+            Console.WriteLine();
+        }
+        /// <summary>
+        /// Gather input from the user
+        /// </summary>
+        /// <returns>Age as validated integer</returns>
+        internal static int Validate()
+        {
+            var str = Console.ReadLine();
+            if (!(int.TryParse(str, out var nmbr)) && !(nmbr < 0))
+                Console.WriteLine("Unrecognised input");
+            return nmbr;
+        }
+        #endregion
+    }
+}
---
+++ FieldDayWithOOP3/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace FieldDayWithOOP3
+{
+    internal class Program
+    {
+        /*
+        Sukurkite klasė Person, kuriai per konsruktoriu paduokite firstName ir lastName;
+        Pridėkite Age ir Gender properties. Patikrinkite ar Age yra neneigiamas;
+        Realizuolite metodą Greetings, kuris atspausdins į konsolę: Hello, my name is {firstName} {lastName};
+        Realizuokite metodą GetFullInfo, kuris atspausdins į konsolę:
+
+        Hello, my name is {firstName} {lastName}
+        My age: {Age}
+        My gender: {Gender}
+        DONE 
+        Vartotojas įveda skaičių n. DONE
+        Programa paprašo įvesti n žmonių vardą, pavardę, amžių ir lytį. DONE
+        Programa atspausdina į ekraną visų žmonių  vardą, pavardę, amžių ir lytį, naudojant GetFullInfo metodą. DONE
+        */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter desired number of persons: ");
+            var str = Console.ReadLine();
+            if (!(int.TryParse(str, out var number)))
+                Console.WriteLine("Unrecognised input");
+            var n = number;
+            var persons = new List<Person>();
+            for (var i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter first name: ");
+                var firstName = Console.ReadLine();
+                Console.WriteLine("Enter last name:");
+                var lastName = Console.ReadLine();
+                var person = new Person(firstName, lastName);
+                Console.WriteLine("Enter Age:");
+                person.Age = Person.Validate();
+                Console.WriteLine("Enter gender:");
+                person.Gender = Console.ReadLine();
+                persons.Add(person);
+            }
+            Console.WriteLine("List of persons are: ");
+            for (var i = 0; i < n; i++)
+            {
+                persons[i].GetFullInfo();
+            }
+        }
+    }
+}
